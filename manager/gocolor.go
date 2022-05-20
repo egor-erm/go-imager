c78280d893d4c16e3bda72911be1e2c24d3f3a9d
Ermolay <es.ermolaev2005@gmail.com>
2022-05-20 17:17:27 +0300

    color convert

--- manager/gocolor.go
+++ manager/gocolor.go
@@ -9,21 +9,21 @@ func RGBAtoHEX(c color.RGBA) string {
 	return fmt.Sprintf("#%02X%02X%02X", c.R, c.G, c.B)
 }
 
-func HEXtoRGBA(hex string) (*color.RGBA, error) {
+func HEXtoRGBA(hex string) *color.RGBA {
 	var c *color.RGBA
 	c.A = 255
 	switch len(hex) {
 	case 7:
-		_, err := fmt.Sscanf(hex, "#%02X%02X%02X", &c.R, &c.G, &c.B)
-		return c, err
+		_, _ = fmt.Sscanf(hex, "#%02X%02X%02X", &c.R, &c.G, &c.B)
+		return c
 	case 4:
-		_, err := fmt.Sscanf(hex, "#%1X%1X%1X", &c.R, &c.G, &c.B)
+		_, _ = fmt.Sscanf(hex, "#%1X%1X%1X", &c.R, &c.G, &c.B)
 		c.R *= 17
 		c.G *= 17
 		c.B *= 17
-		return c, err
+		return c
 	default:
-		err := fmt.Errorf("invalid length, must be 7 or 4")
-		return c, err
+		fmt.Printf("invalid length, must be 7 or 4")
+		return c
 	}
 }
