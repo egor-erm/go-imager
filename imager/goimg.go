683da59719ca5475a1e23ccfbd2cde2babcc23cc
Ermolay <es.ermolaev2005@gmail.com>
2024-01-24 13:39:35 +0300

    Alpha bug fix

--- imager/goimg.go
+++ imager/goimg.go
@@ -131,6 +131,11 @@ func (gimg *goimage) DrawImage(img *goimage, x, y int) {
 	for xc := 0; xc < img.GetRawImage().Bounds().Max.X; xc++ {
 		for yc := 0; yc < img.GetRawImage().Bounds().Max.Y; yc++ {
 			r, g, b, a := img.GetPixel(xc, yc).RGBA()
+
+			if a == 0 {
+				continue
+			}
+
 			color := color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)}
 			gimg.SetPixel(x+xc, y+yc, color)
 		}
